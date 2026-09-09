cca98e5997bd59549001df72a887cc21b95009f1
sakshi Channe <sakshichanne56@gmail.com>
2026-09-09 12:33:33 +0530

    Fix URL redirection with username encoding

--- frontend/src/landing_page/login/login.js
+++ frontend/src/landing_page/login/login.js
@@ -51,7 +51,8 @@ function Login() {
 
                // Go to dashboard
             window.location.href =
-               "https://zerodha-clone-2-7k55.onrender.com";
+               "https://zerodha-clone-2-7k55.onrender.com"+
+    encodeURIComponent(data.username);;
 
 
             } else {
